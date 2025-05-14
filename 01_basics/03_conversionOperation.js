ab34350774302eb0d68d306be61df45c29e12c52
A23droid <pillaiaditya2310@gmail.com>
2025-05-14 08:19:01 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = false
+
+// console.log(typeof score);
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber); 
+/* For undefined and alphabet containing str:
+                                NaN => Not a Number 
+                                and 0 for null/false
+                                and 1 for true */
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false = 0
+
+let isLoggedIn = -545
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof(BooleanIsLoggedIn))
+// console.log(BooleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "dssr" => true; "" => false
+
+let someNumber = -099
+let stringNumber = String(someNumber)
+console.log(stringNumber);
