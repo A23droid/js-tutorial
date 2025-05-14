b8b307b2b1a7776a16dfe8ede273bdf05b83b735
A23droid <pillaiaditya2310@gmail.com>
2025-05-14 07:55:15 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14342
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12i12"
+accountCity = "ABaf"
+
+// accountId = 2 // Not allowed -> Use let rather then var ;)
+// Js doesn't care for ; 
+
+accountEmail = "haha@hehe.hoho"
+accountPassword = "122"
+
+console.log(accountPassword)
+console.table([accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // We are using nodeJS not browser
+
+console.log(3
+    +
+    3 // Code readability should be high so generally no ;
+);
+
+
+let name = "aditya"
+let age = 19
+let isLoggedIn = false
+let state
+
+// numbers
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => uniqueness
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null); // object
+
